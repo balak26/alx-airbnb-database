5313aba604d2575da848513177238d3d3fc3d52e
bamlak21 <bamlakgezahegn27@gmail.com>
2025-05-17 13:18:01 +0300

    fix for aggregations

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,13 +1,13 @@
-    -- Total number of bookings made by each user
-    SELECT user_id, COUNT(*) AS total_bookings
-    FROM bookings
-    GROUP BY user_id;
+-- Total number of bookings made by each user
+SELECT user_id, COUNT(*) AS total_bookings
+FROM bookings
+GROUP BY user_id;
 
-    -- Rank properties based on total number of bookings
-    SELECT 
-    property_id,
-    COUNT(*) AS booking_count,
-    ROW_Number() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-    FROM bookings
-    GROUP BY property_id;
+-- Rank properties based on total number of bookings using ROW_NUMBER()
+SELECT 
+  property_id,
+  COUNT(*) AS booking_count,
+  ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM bookings
+GROUP BY property_id;
 
